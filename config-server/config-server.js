bae87b87996e457f1211b390abe245ca5fb71013
raulnguyenbotads <raulnguyenbotads@hotmail.com>
2023-03-15 15:27:33 +0000

    update

--- config-server/config-server.js
+++ config-server/config-server.js
@@ -1,2 +1,2 @@
-const server = "https://263d-2401-c080-1400-7049-5400-4ff-fe59-44e7.ngrok.io";
+const server = "https://262b-2401-c080-1400-7049-5400-4ff-fe59-44e7.ngrok.io";
 // const server = "http://localhost";
